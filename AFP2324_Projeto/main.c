e87a1ff723d5d33013332a2da6580b08609d236c
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-01-11 14:18:22 +0000

    Replace with files working

---
+++ AFP2324_Projeto/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cache.h"
+
+#define MAX_CODES 2000  
+#define MAX_LINE_LENGTH 1024  
+
+int main() {
+    
+    Cache *geocaches = NULL;
+    int geocacheCount = 0;
+    int option;
+    char file[256];
+
+    do {
+        printf("\n\033[1;34m---- GEOCACHING ----\n\033[0m\n");
+        printf("\033[1;36m1. Load\n2. Clear\n3. Quit\n4. List\n5. FoundP\033[0m\n\033[1;35m\n6. Matrix\nChoose an option: \033[0m");
+        scanf("%d", &option);
+        getchar();
+
+        switch(option) {
+            case 1 :
+
+                if(geocacheCount > 0){
+                    printf("\033[31mCache data exists. Please clear it first.\n");  
+                } else {
+                    printf("\n\033[1;33mEnter filename: \033[0m");
+                    fgets(file, sizeof(file), stdin);
+                    file[strcspn(file, "\n")] = 0; 
+                    getGeocaches(file, &geocaches, &geocacheCount);
+                }
+                break;
+            case 2:
+                if(geocacheCount > 0){
+                    printf("\n\033[1;33m CONFIRM?\n\033[0m");
+                    printf("\n1. YES\n2. NO\n");
+                    printf("\n\033[1;35mChoose an option: \033[0m");
+                
+                    scanf("%d", &option);
+
+                    if (option == 1){
+                        printf("\n\033[32mSTATUS: CONFIRMED\n");
+                        clearGeocaches(&geocaches, &geocacheCount);
+                        break;
+                    } else if (option == 2){
+                        printf("\033[32mSTATUS: DENIED");
+                        break;
+                    } else {
+                        printf("\033[31mOK: INVALID");
+                        break;
+                    }
+                } else {
+                    printf("\n\033[31mCache is already clear.\n");
+                
+                }
+                break;
+            case 3:
+                printf("\n\033[1;32mGoodbye! :(\n\033[0m");
+                clearGeocaches(&geocaches, &geocacheCount); 
+                return EXIT_SUCCESS;
+            case 4:
+                listGeocaches(geocaches, geocacheCount);
+                break;
+            case 5:
+                foundPercentage(geocaches, geocacheCount);
+                break;
+            case 6:
+                calculateMatrix81(geocaches, geocacheCount);
+                break;
+            default:
+                printf("Invalid option.\n");
+        }
+    } while (1);
+
+    return EXIT_SUCCESS;
+}
